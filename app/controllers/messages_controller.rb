2be4f30425707abfce6e3017946d98fb1044781c
Daniel Vartanov <dan@vartanov.net>
2021-05-30 19:58:49 +0100

    Add a new message creation form

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,18 @@ class MessagesController < ApplicationController
   def index
     @messages = Message.all
   end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new params.require(:message).permit(:user_id, :content)
+
+    if @message.save
+      redirect_to :messages
+    else
+      render :new
+    end
+  end
 end
