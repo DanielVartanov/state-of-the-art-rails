13a78f88f8c4fdc8c9f8951b9da6e24b3dc5a48d
Daniel Vartanov <dan@vartanov.net>
2021-05-30 17:33:46 +0100

    Add a User and a Message

---
+++ db/migrate/20210530162954_create_messages.rb
@@ -0,0 +1,10 @@
+class CreateMessages < ActiveRecord::Migration[6.1]
+  def change
+    create_table :messages do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :content
+
+      t.timestamps
+    end
+  end
+end
