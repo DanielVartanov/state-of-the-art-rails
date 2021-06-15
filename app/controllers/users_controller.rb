fc0dd390d5506f8b7b20a7339592a2a66c67c4d9
Daniel Vartanov <dan@vartanov.net>
2021-06-15 07:48:07 +0100

    Add a plain form for adding new User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,9 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def create
+    User.create! params.require(:user).permit(:name)
+    redirect_to :users
+  end
 end
