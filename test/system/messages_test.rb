9e7c68bf7bc0942d8a94f65576e9ea67028f539a
Daniel Vartanov <dan@vartanov.net>
2021-06-18 13:25:54 +0100

    Add tests for messages

---
+++ test/system/messages_test.rb
@@ -0,0 +1,34 @@
+require "application_system_test_case"
+
+class MessagesTest < ApplicationSystemTestCase
+  def setup
+    User.create!(name: 'Mr. Test').tap do |user|
+      Message.create! content: 'First!', user: user
+      Message.create! content: 'Second!', user: user
+    end
+
+    User.create! name: 'Mr. Second test'
+  end
+
+  test 'visit messages page' do
+    visit messages_url
+    assert_text '"First!" by Mr. Test'
+    assert_text '"Second!" by Mr. Test'
+  end
+
+  test 'send a new message' do
+    visit messages_url
+
+    within 'turbo-frame#new_message' do
+      fill_in 'Content', with: 'Hello from tests!'
+      click_on 'Create Message'
+
+      select 'Mr. Second test', from: 'Author'
+      fill_in 'Content', with: 'Hello from another test author'
+      click_on 'Create Message'
+    end
+
+    assert_text '"Hello from tests!" by Mr. Test'
+    assert_text '"Hello from another test author" by Mr. Second test'
+  end
+end
